edd7ac3dee358ccffdf4cb36431bd94f37b5d734
kianiebrahim <ebimp4@gmail.com>
2024-12-11 17:48:13 +0330

    admin pannel for subcategory is updated

--- category_module/admin.py
+++ category_module/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
-admin.site.register(models.SubCategory)
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'MainCategory']
+    list_editable = ['MainCategory']
+
+admin.site.register(models.SubCategory, SubCategoryAdmin)
 admin.site.register(models.MainCategory)
